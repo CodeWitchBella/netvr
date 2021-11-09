5f2c4587196b15e00ee4718a515685e2cef46527
Isabella Skořepová <isabella@skorepova.info>
2021-11-09 17:16:46 +0100

    IsblNet docs

--- netvr-unity/Assets/IsblNet.cs
+++ netvr-unity/Assets/IsblNet.cs
@@ -6,14 +6,33 @@ using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 
-public class IsblNet : IDisposable
+/// <summary>
+/// Main implementation of network interface
+/// </summary>
+/// Do not construct this directly, use .Instance accessor instead.
+public sealed class IsblNet : IDisposable
 {
-    public static IsblNet Instance { get { return IsblNetComponent.Instance; } }
+    /// <summary>
+    /// Makes sure that there is existing instance of IsblNet. This is usually
+    /// not necessary to call directly because .Instance already does that.
+    /// </summary>
+    /// This method is used in Singleton instantiation scheme and is the only
+    /// place where IsblNet constructor is called.
+    public static void EnsureInstanceExists() { if (!IsblNetComponent.InstanceExists) IsblNetComponent.Instance = new(); }
+
+    /// <summary>
+    /// When you need to send or recieve message via network use this to access
+    /// primary IsblNet instance
+    /// </summary>
+    public static IsblNet Instance { get { EnsureInstanceExists(); return IsblNetComponent.Instance; } }
 
     public const int ServerPort = 10000;
     readonly UdpClient _client;
 
-    public IsblNet()
+    /// <summary>Private constructor. Only place where this is called is from
+    /// EnsureInstanceExists and that only happens if other instance does not
+    /// exist already.</summary>
+    IsblNet()
     {
         _client = new UdpClient("127.0.0.1", ServerPort);
         UDPTest();
--- netvr-unity/Assets/IsblNetComponent.cs
+++ netvr-unity/Assets/IsblNetComponent.cs
@@ -3,20 +3,45 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
+/// <summary>
+/// This Unity Component makes sure that there is only a single IsblNet instance
+/// in existence at the same time. There is no reason to access this directly
+/// from code. Use IsblNet.Instance instead.
+/// </summary>
+///
+/// It also provides GUI with a few extra bits of information and makes sure
+/// that IsblNet gets properly disposed.
 public class IsblNetComponent : MonoBehaviour
 {
     static IsblNetComponent _instance;
+    /// <summary>
+    /// Unity part of IsblNet singleton scheme.
+    /// </summary>
     public static IsblNet Instance
     {
         get
         {
-            if (_instance != null) return _instance._net;
-            _instance = FindObjectOfType<IsblNetComponent>();
-            if (_instance != null) return _instance._net;
-            GameObject go = new("IsblNet");
-            _instance = go.AddComponent<IsblNetComponent>();
-            if (_instance._net == null) _instance._net = new();
+            if (_instance?._net == null) IsblNet.EnsureInstanceExists();
             return _instance._net;
+        }
+        set
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<IsblNetComponent>();
+            }
+
+            if (_instance == null)
+            {
+                GameObject go = new("IsblNet");
+                _instance = go.AddComponent<IsblNetComponent>();
+            }
+
+            if (_instance._net != value)
+            {
+                _instance._net?.Dispose();
+            }
+            _instance._net = value;
         }
     }
     public static bool InstanceExists { get { return _instance != null; } }
@@ -34,10 +59,6 @@ public class IsblNetComponent : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject); // only explicit destroying
-        if (_net == null)
-        {
-            _net = new IsblNet();
-        }
     }
 
     void OnDestroy()
