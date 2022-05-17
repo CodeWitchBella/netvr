961cbcd4582face0b78c2f399ec7f187700cda21
Isabella Skořepová <isabella@skorepova.info>
2022-05-17 22:16:29 +0200

    yolo rust usage

--- netvr-rust/src/lib.rs
+++ netvr-rust/src/lib.rs
@@ -1,11 +1,23 @@
 use std::os::raw::c_char;
 
+// region: logger
+static mut LOGGER: unsafe extern "C" fn(*const c_char) = log_fallback;
+
+extern "C" fn log_fallback(_arg: *const c_char) {}
+
+fn log(s: &str) {
+    unsafe {
+        LOGGER(s.as_ptr() as *const c_char);
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn netvr_set_logger(
     func: unsafe extern "C" fn(*const c_char)
 ) {
     println!("Hello world from Rust!");
-    unsafe {
-        func("hello 2".as_ptr() as *const c_char);
-    }
+    unsafe { LOGGER = func; }
+    log("Hello there");
 }
+// endregion: logger
+
