6fc7651cf62463dce91b8a1a742ff5c2386f8941
Isabella Skořepová <isabella@skorepova.info>
2022-06-10 23:19:12 +0200

    only copy release libraries

--- netvr-rust/isbl_netvr_rust/post_build.rs
+++ netvr-rust/isbl_netvr_rust/post_build.rs
@@ -4,6 +4,7 @@ use std::path::{Path, PathBuf};
 
 fn main() {
     let build_path_string = env::var("CRATE_OUT_DIR").unwrap();
+
     let build_path = Path::new(&build_path_string);
     let pdb_dir = build_path.join("pdb");
     // cleanup old pdbs
@@ -37,7 +38,6 @@ fn main() {
     let root = Path::new(&env::var("CRATE_MANIFEST_DIR").unwrap())
         .join("..")
         .join("..");
-
     let copy = |filename: &str, dir1: &str, dir2: &str| {
         let _ = fs::copy(
             build_path.join(filename),
@@ -50,6 +50,10 @@ fn main() {
         );
     };
 
-    copy("isbl_netvr_rust.dll", "Windows", "x64");
-    copy("libisbl_netvr_rust.so", "Android", "arm64-v8a");
+    let profile = env::var("CRATE_PROFILE").unwrap();
+    if profile == "release" {
+        println!("Copying dynamic libraries to unity project");
+        copy("isbl_netvr_rust.dll", "Windows", "x64");
+        copy("libisbl_netvr_rust.so", "Android", "arm64-v8a");
+    }
 }
