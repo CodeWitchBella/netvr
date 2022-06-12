0ad2a3cfbd75789e21e8e794ccae3d74b8cee3d8
Isabella Skořepová <isabella@skorepova.info>
2022-06-12 22:26:12 +0200

    make sure that /pdb flag always changes

--- netvr-rust/isbl_netvr_rust/build.rs
+++ netvr-rust/isbl_netvr_rust/build.rs
@@ -10,7 +10,22 @@ fn main() {
     let profile = env::var("PROFILE").unwrap();
 
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+
     if target_os == "windows" && profile == "debug" {
+        // windows' MSVC hard-codes path to debug info (.pdb) files upon linking
+        // it becomes locked when rust tries to produce stack trace for log/error/panic
+        // there is also no simple way to unlock the PDB file when unloading related dll
+        //
+        // ...this combined means that if I want debug info without having to
+        // restart unity every time I use said info, I have to choose unique pdb
+        // filename for each compilation.
+        //
+        // The way I generate the name might break in the event of time change
+        // (eg. after NTP sync), but if it breaks I either fail compilation once
+        // (in case of repeat value), or more likely loose debug info (in case
+        // of time going backwards). Neither of those is a big problem and therefore
+        // I choose to ignore this.
+
         let time = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap()
---
+++ netvr-rust/xr_layer/build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::time::SystemTime;
+
+fn main() {
+    let profile = env::var("PROFILE").unwrap();
+
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target_os == "windows" && profile == "debug" {
+        // this is a hack to make sure that build.rs script reruns when files in
+        // this crate changes
+
+        let time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        println!("cargo:time={}", time);
+    }
+}
