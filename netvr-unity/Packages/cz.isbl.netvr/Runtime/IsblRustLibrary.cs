c4c7f313db3b4d1a6a3c61b64e4d81300036bef4
Isabella Skořepová <isabella@skorepova.info>
2022-11-12 21:08:23 +0100

    fix formatting

--- netvr-unity/Packages/cz.isbl.netvr/Runtime/IsblRustLibrary.cs
+++ netvr-unity/Packages/cz.isbl.netvr/Runtime/IsblRustLibrary.cs
@@ -7,44 +7,44 @@ namespace Isbl.NetVR
 {
 
 
-class IsblRustLibrary : IDisposable
-{
-    const string LibraryName = "isbl_netvr_rust";
+    class IsblRustLibrary : IDisposable
+    {
+        const string LibraryName = "isbl_netvr_rust";
 
-    private readonly IsblDynamicLibrary _l;
+        private readonly IsblDynamicLibrary _l;
 
-    public delegate void Logger_Delegate(Int32 level, [MarshalAs(UnmanagedType.LPStr)] string message, [MarshalAs(UnmanagedType.LPStr)] string stack);
-    public delegate void SetLogger_Delegate(Logger_Delegate logger);
-    public readonly SetLogger_Delegate SetLogger;
+        public delegate void Logger_Delegate(Int32 level, [MarshalAs(UnmanagedType.LPStr)] string message, [MarshalAs(UnmanagedType.LPStr)] string stack);
+        public delegate void SetLogger_Delegate(Logger_Delegate logger);
+        public readonly SetLogger_Delegate SetLogger;
 
-    public delegate IntPtr HookGetInstanceProcAddr_Delegate(IntPtr func, bool manualUnhook);
-    public readonly HookGetInstanceProcAddr_Delegate HookGetInstanceProcAddr;
+        public delegate IntPtr HookGetInstanceProcAddr_Delegate(IntPtr func, bool manualUnhook);
+        public readonly HookGetInstanceProcAddr_Delegate HookGetInstanceProcAddr;
 
-    public delegate void Unhook_Delegate();
-    public readonly Unhook_Delegate Unhook;
+        public delegate void Unhook_Delegate();
+        public readonly Unhook_Delegate Unhook;
 
-    public delegate void Tick_Delegate(ulong xrInstance);
-    public readonly Tick_Delegate Tick;
-    // ADD_FUNC: add delegate and public field above this line
+        public delegate void Tick_Delegate(ulong xrInstance);
+        public readonly Tick_Delegate Tick;
+        // ADD_FUNC: add delegate and public field above this line
 
 #if !UNITY_EDITOR_WIN
-    [DllImport(LibraryName, EntryPoint = "netvr_set_logger")]
-    static extern void SetLogger_Native(Logger_Delegate logger);
+        [DllImport(LibraryName, EntryPoint = "netvr_set_logger")]
+        static extern void SetLogger_Native(Logger_Delegate logger);
 
-    [DllImport(LibraryName, EntryPoint = "netvr_hook_get_instance_proc_addr")]
-    static extern IntPtr HookGetInstanceProcAddr_Native(IntPtr func, bool manualUnhook);
+        [DllImport(LibraryName, EntryPoint = "netvr_hook_get_instance_proc_addr")]
+        static extern IntPtr HookGetInstanceProcAddr_Native(IntPtr func, bool manualUnhook);
 
-    [DllImport(LibraryName, EntryPoint = "netvr_unhook")]
-    static extern void Unhook_Native();
+        [DllImport(LibraryName, EntryPoint = "netvr_unhook")]
+        static extern void Unhook_Native();
 
-    [DllImport(LibraryName, EntryPoint = "netvr_tick")]
-    static extern void Tick_Native(ulong xrInstance);
-    // ADD_FUNC: add static extern above this line
+        [DllImport(LibraryName, EntryPoint = "netvr_tick")]
+        static extern void Tick_Native(ulong xrInstance);
+        // ADD_FUNC: add static extern above this line
 #endif // !UNITY_EDITOR_WIN
 
-    public IsblRustLibrary()
-    {
-        this._l = new IsblDynamicLibrary(LibraryName, "../netvr-rust/target/debug/");
+        public IsblRustLibrary()
+        {
+            this._l = new IsblDynamicLibrary(LibraryName, "../netvr-rust/target/debug/");
 #if UNITY_EDITOR_WIN
     // get function pointers converted to delegates
     _l.GetDelegate("netvr_set_logger", out SetLogger);
@@ -53,20 +53,20 @@ class IsblRustLibrary : IDisposable
     _l.GetDelegate("netvr_tick", out Tick);
     // ADD_FUNC: add GetDelegate call above this line
 #else
-        SetLogger = SetLogger_Native;
-        HookGetInstanceProcAddr = HookGetInstanceProcAddr_Native;
-        Unhook = Unhook_Native;
-        Tick = Tick_Native;
-        // ADD_FUNC: add a statement above this line
+            SetLogger = SetLogger_Native;
+            HookGetInstanceProcAddr = HookGetInstanceProcAddr_Native;
+            Unhook = Unhook_Native;
+            Tick = Tick_Native;
+            // ADD_FUNC: add a statement above this line
 #endif
-    }
+        }
 
-    public void Dispose()
-    {
-        _l.Dispose();
-    }
+        public void Dispose()
+        {
+            _l.Dispose();
+        }
 
-    public const bool DoesUnload = IsblDynamicLibrary.DoesUnload;
-}
+        public const bool DoesUnload = IsblDynamicLibrary.DoesUnload;
+    }
 
 }
