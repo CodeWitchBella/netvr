9ec0257ecb5e61b11116ca6259783cdb87b2e029
Isabella Skořepová <isabella@skorepova.info>
2023-02-25 20:14:52 +0100

    split out sized value iterator

--- netvr-rust/xr_layer/src/xr_struct_chain.rs
+++ netvr-rust/xr_layer/src/xr_struct_chain.rs
@@ -1,6 +1,4 @@
-use std::fmt;
-
-use crate::{xr_struct::XrStruct, XrDebug, XrDebugValue};
+use crate::xr_struct::XrStruct;
 
 #[derive(Clone)]
 pub struct XrStructChain {
@@ -170,63 +168,5 @@ impl Iterator for XrStructChain {
         let res: *const openxr_sys::BaseInStructure = self.ptr;
         self.ptr = unsafe { (*res).next };
         Some(XrStruct::from(res))
-    }
-}
-
-#[derive(Clone)]
-pub struct SizedArrayValueIterator<T>
-where
-    T: Copy,
-{
-    count: u32,
-    ptr: *const T,
-}
-
-impl<T> SizedArrayValueIterator<T>
-where
-    T: Copy,
-{
-    pub(crate) unsafe fn new(count: u32, ptr: *const T) -> Self {
-        Self { count, ptr }
-    }
-}
-
-impl<T: Copy> Iterator for SizedArrayValueIterator<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<T> {
-        if self.count == 0 {
-            return None;
-        }
-        let ptr = self.ptr;
-        self.ptr = unsafe { ptr.add(1) };
-        self.count -= 1;
-        Some(unsafe { *ptr })
-    }
-}
-
-impl<T> std::fmt::Debug for SizedArrayValueIterator<T>
-where
-    T: Copy + std::fmt::Debug,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut f = f.debug_list();
-        for item in self.clone() {
-            f.entry(&item);
-        }
-        f.finish()
-    }
-}
-
-impl<T> XrDebug for SizedArrayValueIterator<T>
-where
-    T: Copy + XrDebug,
-{
-    fn xr_fmt(&self, f: &mut fmt::Formatter, instance: &openxr::Instance) -> fmt::Result {
-        let mut f = f.debug_list();
-        for item in self.clone() {
-            f.entry(&XrDebugValue(instance.clone(), &item));
-        }
-        f.finish()
     }
 }
