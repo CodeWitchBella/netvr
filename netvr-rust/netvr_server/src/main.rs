a586d9ce359e448377f11844dfe3227a0b5f70c7
Isabella Skořepová <isabella@skorepova.info>
2023-04-10 17:58:52 +0200

    wip

---
+++ netvr-rust/netvr_server/src/main.rs
@@ -0,0 +1,17 @@
+use tokio::runtime::Handle;
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let handle = Handle::current();
+    let responder = libmdns::Responder::spawn(&handle)?;
+    let _svc = responder.register(
+        "_netvr._udp".to_owned(),
+        "NetVR Server".to_owned(),
+        80,
+        &["path=/"],
+    );
+
+    loop {
+        ::std::thread::sleep(::std::time::Duration::from_secs(10));
+    }
+}
