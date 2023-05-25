fa4435b31a9bc548d3969dd2b06533ab5b1a26e3
Isabella Skořepová <isabella@skorepova.info>
2023-05-25 15:49:50 +0200

    delete old server code

--- netvr-server/vendor/cdn.skypack.dev/immer@9.0.12.js
+++
@@ -1,3 +0,0 @@
-// @deno-types="https://cdn.skypack.dev/-/immer@v9.0.12-97oxnW7tND9KBNan3Nc0/dist=es2019,mode=types/dist/immer.d.ts"
-export * from "./immer@9.0.12.proxied.js";
-export { default } from "./immer@9.0.12.proxied.js";
